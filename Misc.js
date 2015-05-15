0bdf2d7acd0328deaaa2c7031558a92134697971
MikailBag <bagishov444.2002.02@gmail.com>
2015-05-15 18:37:53 +0300

    Update Misc.js

--- Misc.js
+++ Misc.js
@@ -1,15 +1,75 @@
-
+"use strict"
  function getCacher(func) {
-      var cache = {}
+      var cache = {};
       return function(x) {
-        if (!(x in cache)) {cache[x] = func.call(this, x)}
-        return cache[x]
+        if (!(x in cache)) {
+         cache[x] = func.call(this, x);
+        }
+        return cache[x];
       }
     }
     
      function getLogger(func, log) {
       return function() {
-        log.push([].slice.call(arguments))
-        return func.apply(this, arguments)
+        log.push([].slice.call(arguments));
+        return func.apply(this, arguments);
       }
     }
+    
+    function applyAll(func) {
+      if (arguments.length == 1) {
+        return func();
+      } else {
+        arguments.shift = [].shift;
+        arguments.shift();
+        var args = [].slice.call(arguments);
+        return func.apply(null, args)
+        }
+      }
+      
+      var NOW = 'только что'
+      var AGO = ' назад'
+      var MIN = ' мин.'
+      var SEC = ' сек.'
+
+      function formatDate(date) {
+        var diff, now, result
+          now = new Date()
+          diff = now.getTime() - date.getTime()
+       
+
+          if (diff <= 1000) {
+            return NOW
+          }
+
+        if (diff < 60 * 1000) {
+          result =  Math.round(diff/1000)
+          return result + SEC + AGO
+        }
+
+        if (diff < 60 * 60 * 1000) {
+         result = Math.round(diff/60000)
+          return result + MIN + AGO
+        }
+
+        return absFormatDate(date)
+      }
+
+      function absFormatDate(date) {
+        var hh = date.getHours();
+        if (hh < 10) hh = '0' + hh;
+
+        var mimi = date.getMinutes();
+        if (mimi < 10) mimi = '0' + mimi;
+
+        var dd = date.getDate();
+        if (dd < 10) dd = '0' + dd;
+
+        var mm = date.getMonth() + 1;
+        if (mm < 10) mm = '0' + mm;
+
+        var yy = date.getFullYear() % 100;
+        if (yy < 10) yy = '0' + yy;
+
+        return dd + '.' + mm + '.' + yy + ' ' + hh + ':' + mimi;
+      }
